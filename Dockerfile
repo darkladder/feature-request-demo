5a673f069463ae9ceb8cf72eaa177e78c9c65cb2
Jeremy Harris <jeremy.harris@zenosmosis.com>
2018-07-30 13:40:37 -0600

    Added npm; prototyping frontend integration

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,19 @@
 FROM python:3.6
 MAINTAINER jeremy.harris@zenosmosis.com
 
-RUN apt-get update \
-    && apt-get install -y libpq-dev postgresql-client postgresql-client-common python3-psycopg2
+RUN curl -sL https://deb.nodesource.com/setup_10.x | bash - \
+  && apt-get update \
+  && apt-get install -y \
+  nodejs \
+  libpq-dev \
+  postgresql-client \
+  postgresql-client-common \
+  python3-psycopg2 \
+  # Clean up apt-cache
+  && rm -rf /var/lib/apt/lists/* \
+  && rm -rf /src/*.deb 
 
+# Install Python project dependencies
 RUN pip install Flask SQLAlchemy psycopg2-binary
 
 WORKDIR /app
@@ -15,6 +25,6 @@ WORKDIR /app
 
 ADD . /app
 
-ENV FLASK_APP=backend/server.py
+ENV FLASK_APP=backend/python/server.py
 
 CMD ["flask", "run", "--host", "0.0.0.0", "--port", "5000"]
--- backend/python/server.py
+++ backend/python/server.py
@@ -11,7 +11,7 @@ from flask import Flask, session, redirect, url_for, escape, request, send_from_
 app = Flask(__name__)
 
 backend_dir = os.path.dirname(os.path.realpath(__file__))
-frontend_dir = os.path.join(backend_dir, '..', 'frontend')
+frontend_dir = os.path.join(backend_dir, '../../', 'frontend')
 
 # Front-end web & asset routing
 @app.route('/', defaults={'path': 'index.html'})
@@ -21,7 +21,7 @@ def index(path):
     return send_from_directory(frontend_dir, path)
 
 # Ex. request: http://localhost:5000/api/feature-request/create?title=hello&client=test-client&client_priority=1&target_date=2018-04-28&product_area=agents
-@app.route('/api/feature-request/create', methods=['POST'])
+@app.route('/api/feature-request', methods=['POST'])
 def createFeatureRequest():
     app.logger.debug(request.args)
     # return "Create a feature request"
@@ -40,3 +40,7 @@ def createFeatureRequest():
         return str(e), 404
 
     return "ok"
+
+@app.route('/api/feature-request/<int:feature_request_id>', methods=['GET'])
+def getFeatureRequest():
+    app.logger.debug(request.args)
